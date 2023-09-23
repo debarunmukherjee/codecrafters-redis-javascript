556949030d09e9f26d336ac5f65db15bdb1a2227
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2023-09-23 19:02:46 +0530

    pass 6th stage

--- app/main.js
+++ app/main.js
@@ -3,23 +3,20 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
+map = {};
+
 const server = net.createServer((connection) => {
     connection.on("data", (data) => {
         const stringData = data.toString();
-        // console.log(stringData);
+        console.log(stringData)
         if (stringData.startsWith("*2\r\n$4\r\nECHO\r\n")) {
             const stringRESP = stringData.substring("*2\r\n$4\r\nECHO\r\n".length, stringData.length)
-            const len = stringRESP.substring(1, stringRESP.indexOf("\r"));
-            const ans = stringRESP.substring(stringRESP.indexOf("\n")+1, stringRESP.indexOf("\n")+1+Number(len))+"\r\n";
             connection.write(stringRESP);
         } else if (stringData.startsWith("*2\r\n$4\r\necho\r\n")) {
             const stringRESP = stringData.substring("*2\r\n$4\r\necho\r\n".length, stringData.length)
-            const len = stringRESP.substring(1, stringRESP.indexOf("\r"));
-            const ans = stringRESP.substring(stringRESP.indexOf("\n")+1, stringRESP.indexOf("\n")+1+Number(len))+"\r\n";
-            console.log(len);
-            console.log(ans);
             connection.write(stringRESP);
+        } else if (stringData.startsWith("*3\r\n$4\r\nSET\r\n")) {
+
         } else {
             connection.write("+PONG\r\n");
         }
