99fc410a69058ad82c614d6a8bc2681898e81496
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2023-09-23 19:23:37 +0530

    pass 6th stage

--- app/main.js
+++ app/main.js
@@ -8,7 +8,6 @@ map = {};
 const server = net.createServer((connection) => {
     connection.on("data", (data) => {
         const stringData = data.toString();
-        console.log(stringData)
         if (stringData.startsWith("*2\r\n$4\r\nECHO\r\n")) {
             const stringRESP = stringData.substring("*2\r\n$4\r\nECHO\r\n".length, stringData.length)
             connection.write(stringRESP);
@@ -16,14 +15,18 @@ const server = net.createServer((connection) => {
             const stringRESP = stringData.substring("*2\r\n$4\r\necho\r\n".length, stringData.length)
             connection.write(stringRESP);
         } else if (stringData.startsWith("*3\r\n$3\r\nset\r\n")) {
-            const stringResp = stringData.substring("*2\r\n$4\r\necho\r\n".length, stringData.length)
+            const stringResp = stringData.substring("*2\r\n$3\r\necho\r\n".length, stringData.length)
             const keyLen = stringResp.substring(1, stringResp.indexOf("\r"));
             const key = stringResp.substring(stringResp.indexOf("\n")+1, stringResp.indexOf("n")+1+Number(keyLen));
+            console.log(keyLen);
+            console.log(key);
+
             const valueResp = stringResp.substring(stringResp.indexOf("n")+1+Number(keyLen)+2);
             const valueLen = valueResp.substring(1, stringResp.indexOf("\r"));
             const value = valueResp.substring(valueResp.indexOf("\n")+1, valueResp.indexOf("n")+1+Number(valueLen));
-            console.log(key)
-            console.log(value)
+            console.log(valueLen);
+            console.log(value);
+
             map[key] = value;
             connection.write("+OK\r\n");
         } else {
