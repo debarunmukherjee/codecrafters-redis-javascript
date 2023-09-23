89d025d9b59e014467a25aff10b8e30ff311772a
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2023-09-23 18:45:15 +0530

    pass 5th stage

--- app/main.js
+++ app/main.js
@@ -8,13 +8,13 @@ const server = net.createServer((connection) => {
     connection.on("data", (data) => {
         const stringData = data.toString();
         console.log(stringData);
-        if (stringData.startsWith("*2\\r\\n$4\\r\\nECHO\\r\\n")) {
-            const stringRESP = stringData.substring("*2\\r\\n$4\\r\\nECHO\\r\\n".length, stringData.length)
-            const len = stringRESP.substring(1, stringRESP.indexOf("\\r"));
+        if (stringData.startsWith("*2\r\n$4\r\nECHO\r\n")) {
+            const stringRESP = stringData.substring("*2\r\n$4\r\nECHO\r\n".length, stringData.length)
+            const len = stringRESP.substring(1, stringRESP.indexOf("\r"));
             connection.write(stringRESP.substring(stringRESP.indexOf("n")+1, stringRESP.indexOf("n")+1+Number(len))+"\r\n");
-        } else if (stringData.startsWith("*2\\r\\n$4\\r\\necho\\r\\n")) {
-            const stringRESP = stringData.substring("*2\\r\\n$4\\r\\necho\\r\\n".length, stringData.length)
-            const len = stringRESP.substring(1, stringRESP.indexOf("\\r"));
+        } else if (stringData.startsWith("*2\r\n$4\r\necho\r\n")) {
+            const stringRESP = stringData.substring("*2\r\n$4\r\necho\r\n".length, stringData.length)
+            const len = stringRESP.substring(1, stringRESP.indexOf("\r"));
             connection.write(stringRESP.substring(stringRESP.indexOf("n")+1, stringRESP.indexOf("n")+1+Number(len))+"\r\n");
         } else {
             connection.write("+PONG\r\n");
