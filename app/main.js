c8fadc1d2546aa208f86dff183721c0cad2568fe
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2023-09-23 19:19:25 +0530

    pass 6th stage

--- app/main.js
+++ app/main.js
@@ -15,8 +15,16 @@ const server = net.createServer((connection) => {
         } else if (stringData.startsWith("*2\r\n$4\r\necho\r\n")) {
             const stringRESP = stringData.substring("*2\r\n$4\r\necho\r\n".length, stringData.length)
             connection.write(stringRESP);
-        } else if (stringData.startsWith("*3\r\n$4\r\nSET\r\n")) {
-
+        } else if (stringData.startsWith("*3\r\n$4\r\nset\r\n")) {
+            const stringResp = stringData.substring("*2\r\n$4\r\necho\r\n".length, stringData.length)
+            const keyLen = stringResp.substring(1, stringResp.indexOf("\r"));
+            const key = stringResp.substring(stringResp.indexOf("\n")+1, stringResp.indexOf("n")+1+Number(keyLen));
+            const valueResp = stringResp.substring(stringResp.indexOf("n")+1+Number(keyLen)+2);
+            const valueLen = valueResp.substring(1, stringResp.indexOf("\r"));
+            const value = valueResp.substring(valueResp.indexOf("\n")+1, valueResp.indexOf("n")+1+Number(valueLen));
+            console.log(key)
+            console.log(value)
+            map[key] = value;
         } else {
             connection.write("+PONG\r\n");
         }
