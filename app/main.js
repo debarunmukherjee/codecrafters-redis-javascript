57a8a7e7f9418b75223ac8afd38f4bed7cfcf81a
debarunmukherjee <debarun.mukherjee1997@gmail.com>
2023-09-23 18:43:01 +0530

    pass 5th stage

--- app/main.js
+++ app/main.js
@@ -8,7 +8,11 @@ const server = net.createServer((connection) => {
     connection.on("data", (data) => {
         const stringData = data.toString();
         console.log(stringData);
-        if (stringData.startsWith("*2\\r\\n$4\\r\\nECHO\\r\\n") || stringData.startsWith("*2\\r\\n$4\\r\\necho\\r\\n")) {
+        if (stringData.startsWith("*2\\r\\n$4\\r\\nECHO\\r\\n")) {
+            const stringRESP = stringData.substring("*2\\r\\n$4\\r\\nECHO\\r\\n".length, stringData.length)
+            const len = stringRESP.substring(1, stringRESP.indexOf("\\r"));
+            connection.write(stringRESP.substring(stringRESP.indexOf("n")+1, stringRESP.indexOf("n")+1+Number(len))+"\r\n");
+        } else if (stringData.startsWith("*2\\r\\n$4\\r\\necho\\r\\n")) {
             const stringRESP = stringData.substring("*2\\r\\n$4\\r\\necho\\r\\n".length, stringData.length)
             const len = stringRESP.substring(1, stringRESP.indexOf("\\r"));
             connection.write(stringRESP.substring(stringRESP.indexOf("n")+1, stringRESP.indexOf("n")+1+Number(len))+"\r\n");
@@ -17,5 +21,5 @@ const server = net.createServer((connection) => {
         }
     })
 });
-
+// *2\r\n$4\r\necho\r\n$5\r\nworld\r\n
 server.listen(6379, "127.0.0.1");
